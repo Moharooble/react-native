9df6ca5d03087d32708efe4d40cef1f6caeefa76
Eng Rooble <maxammed732@gmail.com>
2025-03-08 17:16:03 +0300

    userInfo id

--- app/users/[id].jsx
+++ app/users/[id].jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams} from 'expo-router'
 
 const UserInfo = () => {
+  const info = useLocalSearchParams()
   return (
     <View>
-      <Text>Usernfo</Text>
+      <Text>UserInfo {info.id}</Text>
     </View>
   )
 }
